360a75edded08c4966be83578aea44772db0808d
Nhat Truong Duc <truong.duc.nhat@framgia.com>
2013-12-21 09:45:10 +0700

    Improve the README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleProj::Application.config.secret_key_base = 'fa155a645015cb258b7bfc056de0b689450ad46d34a885a7351a22f4c7fb73f2e4f78269475139130aba78a702e056607eb5f179b5194a312b3aaef27abeb552'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleProj::Application.config.session_store :cookie_store, key: '_sample_proj_session'
