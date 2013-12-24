4e57a802e312939e0eec9aeb4cbee5fc16384f1e
Nhat Truong Duc <truong.duc.nhat@framgia.com>
2013-12-24 11:47:17 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 SampleProj::Application.routes.draw do
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers      #/users/1/following and /users/1/followers
+    end
+    # collection do
+    #   get :tigers                   #/users/tigers
+    # end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
